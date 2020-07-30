159c48958be092ff4f196360d3e97bbdab4835a8
SATyAM JAGTAP <satyam.jagtap1@gmail.com>
2020-07-30 22:53:27 +0530

    initial one

---
+++ week2/Day-4/AnonymousandIIFE/que3.js
@@ -0,0 +1,30 @@
+//Sum of all numbers in an array
+
+//Anonymous function
+var res=function(arr)
+{
+ arr=[23,43,54,56,33,12,21];
+ var sum=0;
+
+for(var i in arr)
+{
+    sum+=arr[i];
+}
+console.log(sum);
+}
+res();
+
+//IIFE
+
+(function(){
+    arr=[23,43,54,56,33,12,21];
+    var sum=0;
+    for(var i in arr)
+    {
+        sum+=arr[i];
+    }
+    console.log(sum);
+
+
+})
+();
