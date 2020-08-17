6e192fc4de842a884389ebacb3daa1b7d10a369e
SATyAM JAGTAP <satyam.jagtap1@gmail.com>
2020-08-17 21:26:56 +0530

    Done

---
+++ week5/Day-1/callback.js
@@ -0,0 +1,69 @@
+
+
+                    setTimeout(function() {
+                       document.getElementById('time-here').innerText=10;
+                       
+                    
+                        setTimeout(function() {
+                            console.log("09");
+                            document.getElementById('time-here').innerText=9;
+                            
+
+                            setTimeout(function() {
+                                console.log("08");
+                                document.getElementById('time-here').innerText=8;
+                                
+
+                                setTimeout(function() {
+                                    console.log("07");
+                                 document.getElementById('time-here').innerText=7;
+                                 
+
+                                    setTimeout(function() {
+                                        console.log("06");
+                                        document.getElementById('time-here').innerText=6
+                                        
+                                        
+                                        setTimeout(function() {
+                                            console.log("05");
+                                            document.getElementById('time-here').innerText=5;
+                                            
+                                            
+                                            setTimeout(function() {
+                                                console.log("04");
+                                                document.getElementById('time-here').innerText=4
+                                                
+                                                setTimeout(function() {
+                                                    console.log("03");
+                                            
+                                                      document.getElementById('time-here').innerText=3
+                                                    setTimeout(function() {
+                                                        console.log("02");
+                                                          document.getElementById('time-here').innerText=2
+                       
+                                                          
+                                                        
+                                                        setTimeout(function() {
+                                                            console.log("01");
+                        document.getElementById('time-here').innerText=1;
+                            document.getElementById('time-here').style.display='none';
+                            document.getElementById("label-name").style.display="none";
+                        document.getElementById('Final-output').innerText="Happy Independance Day..!!";
+                                                            
+                        document.getElementById('image1').innerHTML= "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/1200px-Ashoka_Chakra.svg.png\" width=\'200px\' height=\'200px\' left=\'200px\'>";
+                              
+                        
+
+
+                                                        }, 1000);
+                                                    }, 1000);
+                                                }, 1000);
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+            
+
