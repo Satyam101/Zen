0ed8016738520324b49620b6b5a28eb3ad8afb04
SATyAM JAGTAP <satyam.jagtap1@gmail.com>
2020-08-17 21:49:50 +0530

    Done

--- week5/Day-1/callback.js
+++ week5/Day-1/callback.js
@@ -48,7 +48,7 @@
                         document.getElementById('time-here').innerText=1;
                             document.getElementById('time-here').style.display='none';
                             document.getElementById("label-name").style.display="none";
-                        document.getElementById('Final-output').innerText="Happy Independance Day..!!";
+                        document.getElementById('Final-output').innerText="Happy Independence Day..!!";
                                                             
                         document.getElementById('image1').innerHTML= "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/1200px-Ashoka_Chakra.svg.png\" width=\'200px\' height=\'200px\' left=\'200px\'>";
                               
